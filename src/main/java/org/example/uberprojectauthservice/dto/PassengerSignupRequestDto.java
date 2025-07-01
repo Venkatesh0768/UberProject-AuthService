b94550224b885e91b64d81312b00eca589b2ace6
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-07-01 22:00:34 +0530

    add a auth controller and also db migration of the mysql and add phone_number email password

---
+++ src/main/java/org/example/uberprojectauthservice/dto/PassengerSignupRequestDto.java
@@ -0,0 +1,18 @@
+package org.example.uberprojectauthservice.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerSignupRequestDto {
+
+   private  String email;
+   private String password;
+   private String phoneNumber;
+   private String name;
+
+}
